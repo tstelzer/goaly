c57ddca7801df6f9f5865fe678a09f097a9e2828
Timm Stelzer <timmstelzer@gmail.com>
2017-08-11 20:45:00 +0200

    Add scaffold files.

---
+++ webpack.dev.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].app.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve(__dirname, 'src')],
+        enforce: 'pre',
+        use: [
+          { loader: 'source-map-loader' },
+          { loader: 'babel-loader', options: { cacheDirectory: true } },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: [
+          { loader: 'style-loader', options: { sourceMap: true } },
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1} },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+    modules: [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    port: 3500,
+    hot: true,
+    stats: 'minimal',
+  },
+  devtool: 'cheap-eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new ExtractTextPlugin('style.css'),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
---
+++ webpack.dist.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].app.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: [
+          { loader: 'babel-loader', options: { cacheDirectory: true } },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader', 
+            'sass-loader',
+          ]
+        }),
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+    modules: [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false,
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true,
+        booleans: true,
+      }
+    }),
+  ],
+};
