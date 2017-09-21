7c6433964029bb0948408fb7ebed589514b33fbf
Timm Stelzer <timmstelzer@gmail.com>
2017-09-21 12:28:40 +0200

    Repetitions: Add basic selectors.

--- src/modules/repetitions/repetitions-selectors.ts
+++ src/modules/repetitions/repetitions-selectors.ts
@@ -1,6 +1,8 @@
 import {createSelector} from 'reselect'
+import {values} from 'ramda'
 
-import {DOMAIN} from './repetitions-constants'
 import {Repetition} from './repetitions-model'
+import {State, ById, AllIds} from 'modules/core'
 
-export const allReps = (state: any) => state[DOMAIN]
+export const getEntities = (state: State<Repetition>): ById<Repetition> => state.byId
+export const getIds = (state: State<Repetition>): AllIds => state.allIds
