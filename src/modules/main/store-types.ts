ece29be7c3c5000ee01ffed073eda264a5415ef8
Timm Stelzer <timmstelzer@gmail.com>
2017-09-24 13:57:39 +0200

    Main: Clean up imports.

--- src/modules/main/store-types.ts
+++ src/modules/main/store-types.ts
@@ -1,12 +1,12 @@
 import {StateSlice} from 'modules/core'
-import {model as setRepsModel} from 'modules/set-reps/'
-import {model as workoutsModel} from 'modules/workouts/'
-import {model as setsModel} from 'modules/sets/'
-import {model as repetitionsModel} from 'modules/repetitions/'
+import * as setReps from 'modules/set-reps/'
+import * as workouts from 'modules/workouts/'
+import * as sets from 'modules/sets/'
+import * as repetitions from 'modules/repetitions/'
 
 export interface Store {
-  readonly setReps: StateSlice<setRepsModel.SetRep>,
-  readonly sets: StateSlice<setsModel.Set>,
-  readonly repetitions: StateSlice<repetitionsModel.Repetition>,
-  readonly workouts: StateSlice<workoutsModel.Workout>,
+  readonly setReps: StateSlice<setReps.model.SetRep>,
+  readonly sets: StateSlice<sets.model.Set>,
+  readonly repetitions: StateSlice<repetitions.model.Repetition>,
+  readonly workouts: StateSlice<workouts.model.Workout>,
 }
