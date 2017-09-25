c3fa69f0af40776b5496627d7cb7624441332494
Timm Stelzer <timmstelzer@gmail.com>
2017-09-25 21:47:50 +0200

    Main: Add some fluff to crisp up the browser tab.

--- src/modules/sets/sets-model.ts
+++ src/modules/sets/sets-model.ts
@@ -1,3 +1,7 @@
+/**
+ * @prop {String} description A short description of the set.
+ * @prop {String} name A terse identifier for the set.
+ */
 export interface Set {
     readonly id: string
     readonly name?: string
