c51eacf3f857364824344ae26cb6f257925c06b8
Timm Stelzer <timmstelzer@gmail.com>
2017-12-09 21:34:00 +0100

    Fix HMR and build speeds.

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,21 +2,26 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].app.js',
-    publicPath: "",
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: [
+          { loader: 'react-hot-loader/webpack'},
           { loader: 'source-map-loader' },
-          { loader: 'awesome-typescript-loader'},
+          { loader: 'awesome-typescript-loader' },
         ]
       },
       {
@@ -28,36 +33,32 @@ module.exports = {
           { loader: 'postcss-loader', options: { sourceMap: true } },
         ],
       },
-      {
-        test: /\.scss$/,
-        include: [path.resolve(__dirname, 'src')],
-        use: [
-          { loader: 'style-loader', options: { sourceMap: true } },
-          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 2, modules: false} },
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } },
-        ],
-      },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx", ".scss", ".ts", ".tsx", ".css"],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".css"],
     modules: [
       path.resolve(__dirname, 'src'),
       path.resolve(__dirname, 'node_modules'),
     ],
+    symlinks: false,
+    cacheWithContext: false,
   },
   devServer: {
+    open: false,
     historyApiFallback: true,
     compress: true,
     port: 3500,
     hot: true,
-    stats: 'minimal',
+    stats: 'errors-only',
   },
-  devtool: 'cheap-eval-source-map',
+  devtool: 'eval',
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new ExtractTextPlugin('style.css'),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HardSourceWebpackPlugin({
+      cacheDirectory: '.cache/hard-source/[confighash]'
+    }),
   ],
 };
